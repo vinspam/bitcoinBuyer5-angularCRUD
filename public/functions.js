40e818da60f11713a9ee924b7cceed012e89cfac
vinspam <brandonconnor0314@gmail.com>
2019-08-06 22:07:06 -0600

    db conected with update function, sign-in welcom notification

--- public/functions.js
+++ public/functions.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContenetLoaded", event => {
     const app = firebase.app();
-    console.log(app)
+    console.log(app);
+    const db = firebase.firestore();
+    const newUser = db.collection('users').doc('1');
+
+    // newUser.get()
+    //     .then(doc => {
+    //         const data = doc.data();
+    //         console.log(`User Name: ${data.name}<br>Active Member: ${data.isActive}<br>Email: ${data.email}<br>`)
+    //             // document.getElementById("new-users").innerHTML = `User Name: ${data.name}<br>Active Member: ${data.isActive}<br>Email: ${data.email}<br>`
+    //     });
+    newUser.onSnapshot(doc => {
+        const data = doc.data();
+        document.getElementById("db-change").innerHTML = (`User Name: ${data.name}<br>Active Member: ${data.isActive}<br>Email: ${data.email}<br>`)
+    })
 });
 
 function googleLogin() {
@@ -8,7 +21,7 @@ function googleLogin() {
     firebase.auth().signInWithPopup(provider)
         .then(result => {
             const user = result.user;
-            document.write(`Welcome to your Armchair, Bitcoinist ${user.displayName}`);
+            document.getElementById("new-user").innerHTML = (`Welcome to your Analyst Console,<br /> Bitcoinist ${user.displayName}`);
             console.log(user)
         })
         .catch(console.log)
