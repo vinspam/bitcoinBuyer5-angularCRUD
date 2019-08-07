29993db967f191dfbcb3c7bf07b3070b026ca490
vinspam <brandonconnor0314@gmail.com>
2019-08-07 16:55:07 -0600

    serverless messageing function, upload function

--- public/functions.js
+++ public/functions.js
@@ -3,19 +3,19 @@ document.addEventListener("DOMContentLoaded", event => {
     // console.log(app);
     const db = firebase.firestore();
     const newUser = db.collection('users').doc('1');
-    // static
+    // static GET
     newUser.get()
         .then(doc => {
             const data = doc.data();
             console.log(data)
             document.getElementById("db-change").innerHTML = (`<strong>Name: </strong>${data.name}<br><strong>Email: </strong>${data.email}<br><strong>Membership: </strong>${data.groupName}<br><strong>Active Member: </strong>${data.isActive}`);
         });
-    // real-time
+    // real-time GET
     newUser.onSnapshot(doc => {
-        const data = doc.data();
-        document.getElementById("db-update").innerHTML = (`<strong>Name: </strong>${data.name}<br><strong>Email: </strong>${data.email}<br><strong>Membership: </strong>${data.groupName}`)
-    })
-
+            const data = doc.data();
+            document.getElementById("db-update").innerHTML = (`<strong>Name: </strong>${data.name}<br><strong>Email: </strong>${data.email}<br><strong>Membership: </strong>${data.groupName}`)
+        })
+        // query GET
     const allUsers = db.collection('users');
     const query = allUsers.where('isActive', '==', true)
     query.get()
@@ -26,6 +26,10 @@ document.addEventListener("DOMContentLoaded", event => {
 
             })
         })
+
+    // query SORT 
+    // const queryOrderBy = allUsers.orderBy('dateOfBirth', 'desc').limit(10)
+    // query GET
     const queryInactive = allUsers.where('isActive', '==', false)
     queryInactive.get()
         .then(users => {
@@ -36,21 +40,33 @@ document.addEventListener("DOMContentLoaded", event => {
             })
         })
 });
-
+// Real-time Upload/Download
+function uploadData(files) {
+    const storageRef = firebase.storage().ref();
+    const newRef = storageRef.child('TomPicUpload.jpg');
+    const file = files.item(0);
+    const task = newRef.put(file)
+        //  Real-time Download
+    task.then(snapshot => {
+        console.log(snapshot)
+        const url = snapshot.downloadURL
+        document.getElementById('imgUpload').setAttribute('src', url)
+    })
+}
+// Real-time Edit PUT 
 function updateStatus(e) { // need type-checking
     const db = firebase.firestore();
     const newData = db.collection('users').doc('1');
     // e.target.value = (e === "yes" || "true") ? true : false;
     newData.update({ isActive: e.target.value })
 }
-
+// Real-time Edit PUT  
 function updateData(e) {
     const db = firebase.firestore();
     const newData = db.collection('users').doc('1');
     newData.update({ name: e.target.value })
 }
-
-
+// Google Login Authentication
 function googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
